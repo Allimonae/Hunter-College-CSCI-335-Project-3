bad95527e32f9650b25559af553e7d0de368db0e
Allimonae <128328789+Allimonae@users.noreply.github.com>
2024-04-26 11:27:37 -0400

    trying to do quickselect

--- QuickSelect1.cpp
+++ QuickSelect1.cpp
@@ -1,22 +1,31 @@
 #include <random>
+#include <algorithm>
 #include "QuickSelect1.hpp"
 
 void quickSelect1(const std::string& header, std::vector<int> data){
     std::cout << quickSelectHelper(data);
 }
 
-int quickSelectHelper(std::vector<int> data){
-    std::cout << "hey";
+int quickSelectHelper(std::vector<int> data, int left, int right, int k){
+    if (left + 10 <= right){
+        std::vector<int> pivotarray = {data[data.size()/2], left, right};
+        std::sort(pivotarray.begin(), pivotarray.end());
+        int pivot = pivotarray[1];
+
+        for ( ; ; ){
+            while(data[++i] < pivot){}
+            while(pivot < data[--j]){}
+        }
+    }
+
+    std::cout << "hey: " << data.size() << std::endl;
     if (data.size() == 1){
         return data[0];
     }
-
-    int k = data.size()/2;
-    if (data.size() % 2 == 1){ // if data size is odd
-        k += 1; 
-    }
     
-    int pivot = data[k];
+    int k = data.size()/2;
+    int randnum = rand() % data.size(); // randnum is a random number in data.size()
+    int pivot = data[randnum];
     std::vector<int> partitionA;
     std::vector<int> partitionB;
     for (auto it : data){
@@ -26,11 +35,5 @@ int quickSelectHelper(std::vector<int> data){
         else {
             partitionB.push_back(it);
         }
-    }
-    if (partitionA.size() > k){
-        return quickSelectHelper(partitionA);
-    }
-    else {
-        return quickSelectHelper(partitionB);
     }
 }
